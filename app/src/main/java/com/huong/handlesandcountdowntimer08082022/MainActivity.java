1971003e97ae6327611ec7c392d815febe27c09b
NMH <nm.huong6497@gmail.com>
2022-10-03 07:10:00 +0700

    Handler

--- app/src/main/java/com/huong/handlesandcountdowntimer08082022/MainActivity.java
+++ app/src/main/java/com/huong/handlesandcountdowntimer08082022/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -21,8 +21,8 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.image_4,
             R.drawable.image_5
     };
-    int count = 0;
-    boolean isRunning =false;
+    int count = 1;
+    boolean isRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,34 +38,34 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (!isRunning) {
                     isRunning = true;
-                    countDownTimer = new CountDownTimer(1200, 1000) {
+
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
                         @Override
-                        public void onTick(long l) {
-                            if (l >= 1000) {
-                                count = count == arrImages.length -1 ? 0 : count;
+                        public void run() {
+                            // Pause
+                            if (!isRunning) {
+                                handler.removeCallbacks(this);
+
+                            } else {
+                                // Run
+                                count = count == arrImages.length ? 0 : count;
                                 imageView.setImageResource(arrImages[count++]);
+                                handler.postDelayed(this, 1000);
                             }
                         }
-
-                        @Override
-                        public void onFinish() {
-                            countDownTimer.start();
-                        }
-                    };
-                    countDownTimer.start();
+                    }, 1000);
                 } else {
-                    Toast.makeText(MainActivity.this, "Banner dang chay", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "Banner đang chạy", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
         // Requirement: Nếu đang chạy thì dừng, không thì thông báo
         btnPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (isRunning) {
                     isRunning = false;
-                    countDownTimer.cancel();
                 } else {
                     Toast.makeText(MainActivity.this, "Banner đã dừng", Toast.LENGTH_SHORT).show();
                 }
@@ -73,3 +73,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
